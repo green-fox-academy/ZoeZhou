9b8746680d9b55b6ae35519dacf15f6d03b323c0
ZoeCN <290241128@qq.com>
2017-09-02 16:19:46 +0800

    fix function

--- week-02/day-02/exercise-12/exercise-12.js
+++ week-02/day-02/exercise-12/exercise-12.js
@@ -2,7 +2,7 @@
 var height = 100;
 var length = 20;
 var width = 30;
-var surfaceArea = length * width;
+var surfaceArea = length * width * 2 + length * height * 2 + width * height * 2;
 var volume = length * width * height;
 
 console.log('Surfage Area: ' + surfaceArea);
