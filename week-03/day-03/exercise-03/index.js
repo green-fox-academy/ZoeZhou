f43faa51cd1e87473e44c3c79621283e0665c084
ZoeCN <290241128@qq.com>
2017-09-06 20:30:33 +0800

    fix ex3

--- week-03/day-03/exercise-03/index.js
+++ week-03/day-03/exercise-03/index.js
@@ -1,28 +1,49 @@
 window.addEventListener('load', init);
+var url = 'https://wakeful-vision.glitch.me/api/messages';
+var messagesKeeper;
 function init() {
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    'get',
-    'https://wakeful-vision.glitch.me/api/messages'
-  );
-  xhr.send();
-  xhr.addEventListener('readystatechange', function () {
-    if (xhr.readyState === xhr.DONE) {
-      if (xhr.status === 200) {
-        // get json
-        var jsonData = JSON.parse(xhr.responseText);
-        console.log(jsonData);
-        loadPage(jsonData.messages);
-      }
-    }
-  });
+  fetch(url).then(function (response) {
+    response.json().then(function (data) {
+      messagesKeeper = data.messages;
+      loadPage(data.messages);
+    })
+  })
+  var submit = document.getElementById('submit');
+  submit.addEventListener('click', postInfo);
 }
-
 function loadPage(data) {
   var container = document.getElementById('container');
+  container.innerHTML = '';
   for (var i = 0; i < data.length; i++) {
-    var li = document.createElement('li');
-    li.innerText = `name: ${data[i].name}, message: ${data[i].message}`;
-    container.appendChild(li);
+    var message = document.createElement('span');
+    message.innerText = `${data[i].name} : ${data[i].message}`;
+    container.appendChild(message);
+  }
+}
+function postInfo() {
+  var userName = document.getElementById('user-name').value.trim();
+  var inputMsg = document.getElementById('input').value.trim();
+  if (userName.length && inputMsg.length) {
+    var request = {
+      name: userName,
+      message: inputMsg
+    };
+    fetch(url, {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(request)
+    }).then(function (response) {
+      messagesKeeper.push(request);
+      loadPage(messagesKeeper);
+      var container = document.getElementById('container');
+      container.scrollTop = container.scrollHeight;
+    }).catch(function (error) {
+      console.log(error);
+      return;
+    });
+  } else {
+    return;
   }
 }
