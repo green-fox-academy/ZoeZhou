f8cbef462050fca0edc44b6679580039fe51bf6a
ZoeCN <290241128@qq.com>
2017-09-20 18:05:21 +0800

    week-5-day-3

---
+++ week-05/day-03/exercise-05/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src') + '/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+}
